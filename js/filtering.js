d0ce2ea0f7dece1bbdbf2755080a358329b2df74
coinhojae <hojae.kr@gmail.com>
2017-12-19 04:55:05 +0900

    hi

--- js/filtering.js
+++ js/filtering.js
@@ -1,6 +1,7 @@
 $(".dropdown-menu li a").click(function(){
 	$(this).parents(".dropdown").find('.btn').html($(this).text() + ' <span class="caret"></span>');
 	$(this).parents(".dropdown").find('.btn').val($(this).data('value'));
+	console.log('aaaa');
 	console.log($(this).text());
 	console.log($(this).data('value'));
 });
