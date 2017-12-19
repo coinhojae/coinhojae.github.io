34b1719f724e7f86fbb7362bc1bb0bb1ce03b78b
coinhojae <hojae.kr@gmail.com>
2017-12-19 16:17:06 +0900

    gajua

--- js/maketable.js
+++ js/maketable.js
@@ -1,13 +1,17 @@
+
+
 function maketable(data) {
 	var allRows = data.split(/\r?\n|\r/);
 	var table = '<table class="table table-hover" id="hojae201801">';
 	for (var singleRow = 0; singleRow < allRows.length; singleRow++) {
+        
 		if (singleRow === 0) {
 			table += '<thead>';
 			table += '<tr>';
 		} else {
 			table += '<tr>';
 		}
+        
 		var rowCells = allRows[singleRow].split(',');
 		for (var rowCell = 0; rowCell < rowCells.length; rowCell++) {
 			if (singleRow === 0) {
