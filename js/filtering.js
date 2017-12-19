13b2f49f0ac019749063c9dc3d20ddaa63dbd501
coinhojae <hojae.kr@gmail.com>
2017-12-19 11:41:09 +0900

    11

--- js/filtering.js
+++
@@ -1,6 +0,0 @@
-$(".dropdown-toggle li a").click(function(){
-	$(this).parents(".dropdown-toggle").find('.btn').html($(this).text() + ' <span class="caret"></span>');
-	$(this).parents(".dropdown-toggle").find('.btn').val($(this).data('value'));
-	console.log("$(this).text()");
-	console.log("kjlee");
-});
